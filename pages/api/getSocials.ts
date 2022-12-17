34ec998f24d0e7d030ba65a66a6e8c80d322da95
Natanael <Natanael.jop.app@gmail.com>
2022-12-17 18:21:26 +0100

    Second commit

--- pages/api/getSocials.ts
+++ pages/api/getSocials.ts
@@ -1,6 +1,7 @@
 import { groq } from "next-sanity";
 import { sanityClient } from "../../sanity";
 import type { NextApiRequest, NextApiResponse } from "next";
+import { Social } from "../../typing";
 
 const query = groq`
     *[_type == "social"]
@@ -10,12 +11,12 @@ type Data = {
     socials: Social[]
 }
 
-export default function handler (
+export default async function handler (
     rq: NextApiRequest,
     res: NextApiResponse<Data>
 ) {
 
-    const socials: Socaial[] = await sanityClient.fetch(query);
+    const socials: Social[] = await sanityClient.fetch(query);
 
     res.status(200).json({ socials })
-}
+} 
